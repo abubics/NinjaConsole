397b5c55a5363193c5de4f8241d1bfba6dd6d5c5
Andras Bubics <abubics@dius.com.au>
2012-03-05 12:06:07 +1100

    - Merged text pane and command line into NinjaConsole class.

--- LogConstants.java
+++ LogConstants.java
@@ -4,5 +4,7 @@ public interface LogConstants
     public static final int Style_Input = 1;
     public static final int Style_Error = 2;
     public static final int Style_System = 3;
-    public static final int Style_Count = 4;
+    public static final int Style_Interactive = 4;
+    public static final int Style_Debug = 5;
+    public static final int Style_Count = 6;
 }
