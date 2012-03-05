aa4f3cd64ceabac0d3d505f11a5d972edf73326f
Andras Bubics <abubics@dius.com.au>
2012-03-06 09:26:31 +1100

    - Moved into NinjaConsole package.

---
+++ NinjaConsole/LogConstants.java
@@ -0,0 +1,12 @@
+package NinjaConsole;
+
+public interface LogConstants
+{
+    public static final int Style_Output = 0;
+    public static final int Style_Input = 1;
+    public static final int Style_Error = 2;
+    public static final int Style_System = 3;
+    public static final int Style_Interactive = 4;
+    public static final int Style_Debug = 5;
+    public static final int Style_Count = 6;
+}
